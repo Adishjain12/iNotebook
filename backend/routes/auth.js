4ba649cd52f6c39ba6b782ed5b10ea3fb82f4199
Adish Jain <adish12012002@gmail.com>
2022-08-10 19:15:35 +0530

    Creating ThunderClient Collections to Manage Requests

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,25 +1,48 @@
-const express=require('express');
-const router=express.Router();
-const User=require('../models/User');
-const { body, validationResult } = require('express-validator');
- 
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
-router.post('/',[
-    body('name','Minimum length should be 3').isLength({min:3}),
-    body('email','Enter a valid email').isEmail(),
-    body('password','Password length must be atleast 5 characters').isLength({min:5}),
-
-],(req,res )=>{
+// Create a user using: POST "api/auth/createuser". No login required
+router.post(
+  "/createuser",
+  [
+    body("name", "Minimum length should be 3").isLength({ min: 3 }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password length must be atleast 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    //If there are errors return bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-        name: req.body.name,
-        email:req.body.email,
-        password:req.body.password
-      }).then(user => res.json(user)).catch(err=>console.log(err),
-      res.json({error:'Enter unique email',message:err.message}));
-})
 
-module.exports=router;
+    //check whether the user with this email exist
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res.status(400).json({ error: "User already exist" });
+      }
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      });
+      /*.then((user) => res.json(user))
+      .catch(
+        (err) => console.log(err),
+        res.json({ error: "Enter unique email", message: err.message })
+      );*/
+
+      res.json(user);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Sone error occured");
+    }
+  }
+);
+
+module.exports = router;
